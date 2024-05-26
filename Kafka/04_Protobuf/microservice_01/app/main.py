c4e509070b4b274159a358d6ebc12d141f53c26a
hmzi67 <hamzawaheed057@gmail.com>
2024-05-27 01:10:34 +0500

    💨Protobuf configration 💱 added ✔

--- Kafka/04_Protobuf/microservice_01/app/main.py
+++ Kafka/04_Protobuf/microservice_01/app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from sqlmodel import SQLModel, Field
 from contextlib import asynccontextmanager
 from typing import Optional
@@ -6,6 +6,8 @@ from app import settings
 from aiokafka import AIOKafkaProducer, AIOKafkaConsumer
 import asyncio
 import json
+from typing import Annotated
+from app import order_pb2
 
 class Order(SQLModel):
     id: Optional[int] = Field(default=None)
@@ -17,7 +19,7 @@ class Order(SQLModel):
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     print('Creating Tables')
-    task = asyncio.create_task(consume_messages('todos', 'broker:19092'))
+    task = asyncio.create_task(consume_messages("order", 'broker:19092'))
     yield
 
 app: FastAPI = FastAPI(
@@ -26,25 +28,40 @@ app: FastAPI = FastAPI(
     version='1.0.0'
 )
 
+async def get_kafka_producer():
+    producer = AIOKafkaProducer(bootstrap_servers= 'broker:19092')
+    await producer.start()
+    try:
+        yield producer
+    finally:
+        await producer.stop()
+
+
+
+
+
 @app.get('/')
 def root():
     return "HEllo World"
 
 @app.post('/create_order')
-async def read_product(order: Order):
-    producer = AIOKafkaProducer(bootstrap_servers= "broker:19092")
-    await producer.start()
-    order_JSON = json.dumps(order.__dict__).encode('utf-8')
-    print(order_JSON)
+async def read_product(order: Order, producer: Annotated[AIOKafkaProducer, Depends(get_kafka_producer)]):
+
+    order_protobuf = order_pb2.Order(id=order.id, username=order.username, product_id=order.product_id, product_name=order.product_name, product_price=order.product_price)
+    print(f"Protobuf order data: {order_protobuf}")
+
+    serialized_order = order_protobuf.SerializeToString()
+    print(f"Serialized ordere data: {serialized_order}")
+
     # Get cluster layout and initial topic/partition leadership information
     try:
         # Produce message
-        await producer.send_and_wait("order", order_JSON)
+        await producer.send_and_wait("order", serialized_order)
     finally:
         # Wait for all pending messages to be delivered or expire.
         await producer.stop()
 
-    return order_JSON
+    return serialized_order
 
 
 async def consume_messages(topic, bootstrap_servers):
@@ -52,7 +69,7 @@ async def consume_messages(topic, bootstrap_servers):
     consumer = AIOKafkaConsumer(
         topic,
         bootstrap_servers=bootstrap_servers,
-        group_id="my-todos-group",
+        group_id="my-group",
         auto_offset_reset='earliest'
     )
 
@@ -64,6 +81,9 @@ async def consume_messages(topic, bootstrap_servers):
             print(f"Received message: {message.value.decode()} on topic {message.topic}")
             # Here you can add code to process each message.
             # Example: parse the message, store it in a database, etc.
+            new_order = order_pb2.Order()
+            new_order.ParseFromString(message.value)
+            print(f"\n\n Consumer Deserialized data: {new_order}")
     finally:
         # Ensure to close the consumer when done.
         await consumer.stop()
